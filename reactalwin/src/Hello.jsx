1be0599b84e38ad77cbbc9f179ad91e3225f05a6
Alwin <Alwin41>
2024-06-05 16:46:12 -0400

    The new file called hello.jsx is added and app.jsx modified

---
+++ reactalwin/src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloComponent = () => {
+  return (
+    <div>
+      <h1>Hello, welcome to reactAlwin!</h1>
+    </div>
+  );
+};
+
+export default HelloComponent;
